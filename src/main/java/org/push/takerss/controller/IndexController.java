202f182e2c717205a3b0a65bf00f487cf486d932
evgenii-m <mez.e.s@yandex.ru>
2016-03-01 22:32:21 +0300

    Implemented addition of RSS channel.

--- src/main/java/org/push/takerss/controller/IndexController.java
+++ src/main/java/org/push/takerss/controller/IndexController.java
@@ -12,10 +12,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.push.takerss.model.entity.RssChannelEntity;
 import org.push.takerss.model.service.IRssChannelService;
+import org.push.takerss.model.service.impl.RssChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -38,10 +40,23 @@ public class IndexController {
     
     @RequestMapping(method = GET)
     public String index(Model uiModel) {
+        RssChannelEntity rssChannelBlank = new RssChannelEntity();
+        uiModel.addAttribute("rssChannelBlank", rssChannelBlank);
         List<RssChannelEntity> rssChannelList = rssChannelService.getAll();       
         uiModel.addAttribute("rssChannelList", rssChannelList);
         logger.info("Rss Channels count = " + rssChannelList.size());
         return "index";
+    }
+    
+    @RequestMapping(method = POST)
+    public String addRssChannel(@ModelAttribute("rssChannelBlank") RssChannelEntity rssChannel, 
+            Model uiModel, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            logger.warn("Failed to add new RSS channel (" + rssChannel + ")");
+        } else {
+            rssChannelService.save(rssChannel);
+        }
+        return "redirect:/";
     }
     
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,18 +1,25 @@
 <%@ page language="java" contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
-<%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles" %>
-<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 
 <div>
     <c:if test="${not empty rssChannelList}">
-        <c:forEach items="${rssChannelList}" var="rssChannel">
-            <tr>
-                <td>${rssChannel.id}</td>
-                <td>${rssChannel.name}</td>
-                <td>${rssChannel.url}</td>
-            </tr>
-        </c:forEach>
+        <table border="1" cellpadding="10" bordercolor="#111111">
+            <thead>
+                <tr>
+                    <td><b>NAME</b></td>
+                    <td><b>URL</b></td>
+                </tr>
+            </thead>
+            <tbody>
+		        <c:forEach items="${rssChannelList}" var="rssChannel">
+		            <tr>
+		                <td>${rssChannel.name}</td>
+		                <td>${rssChannel.url}</td>
+		            </tr>
+		        </c:forEach>
+	        </tbody>
+        </table>
     </c:if>
 </div>
